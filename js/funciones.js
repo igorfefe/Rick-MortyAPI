cc5dde7428bbe8a3a7f317f259c8c80b3f6d8b72
mverikas <68635436+katakvri@users.noreply.github.com>
2021-06-03 18:30:43 +0200

    1 commit

---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+function getCharacters() {
+    $.ajax({
+        url: "https://rickandmortyapi.com/api/character/5,76,33,23,45,56",
+        method: "GET",
+        dataType: "json",
+        contentType: 'application/x-www-form-urlencoded',
+        success: function (data) {
+            var charactersArray = data;
+            console.log(charactersArray);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+};
+
+console.log(getCharacters());
+
